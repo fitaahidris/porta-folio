41008acdc286dbb138255923b82e18ab6b40df97
fitaahidris <fitaahidrishassan@gmail.com>
2024-10-07 11:18:18 +0300

    porta folio team

---
+++ script.js
@@ -0,0 +1,17 @@
+// const swiper = new Swiper('.slider-wrapper', {
+//     loop: true,
+//     // If we need pagination
+//     pagination:{
+//         else: '.swiper-pagination',
+//     },
+        
+//     // Navigation arrows
+//     navigation:{
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     break{
+//         break
+//     }
+
+//   });
